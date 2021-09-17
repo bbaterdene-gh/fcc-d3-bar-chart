56c9f0ab4f05e3e53eb22fad83d621267443c6f1
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-17 18:21:58 +0800

    added axes

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const drawBarChart = (data) => {
   const graphWidth = 900
   const graphHeight = 460
   const paddingBottom = 50
+  const paddingLeft = 80
+  const paddingRight = 50
+  const paddingTop =50
   const barWidth = 3
   const svg = d3.select('svg')
-                .attr('width', graphWidth)
-                .attr('height', graphHeight + paddingBottom)
+                .attr('width', graphWidth + paddingLeft + paddingRight)
+                .attr('height', graphHeight + paddingBottom + paddingTop)
 
   const xScale = d3.scaleBand()
                    .domain(data.map(d => new Date(d[0])))
@@ -15,21 +18,43 @@ const drawBarChart = (data) => {
                    .domain(d3.extent(data, d => d[1]))
                    .range([graphHeight, 0])
 
-  svg.selectAll('rect')
-     .data(data)
-     .join('rect')
-     .attr('x', d => xScale(new Date(d[0])))
-     .attr('y', d => yScale(d[1]))
-     .attr('width', barWidth)
-     .attr('height', d => graphHeight - yScale(d[1]))
-     .style('fill', '#33adff')
-     .on('mouseover', function() {
-       d3.select(this).style('fill', '#ffffff')
-     })
-     .on('mouseleave', function() {
-       d3.select(this).style('fill', '#33adff')
-    })
-  console.log(data.length)
+  const xAxis = d3.axisBottom()
+                  .scale(xScale)
+                  .tickValues(xScale.domain().filter(d => {
+                    const quarter = Math.floor((d.getMonth() + 3) / 3);
+                    return d.getFullYear() % 5 === 0 && quarter === 1
+                  }))
+                  .tickFormat(d3.timeFormat('%Y'));
+
+  const yAxis = d3.axisLeft()
+                  .scale(yScale)
+
+  const graph = svg.append('g')
+                    .selectAll('rect')
+                    .data(data)
+                    .join('rect')
+                    .attr('x', d => xScale(new Date(d[0])))
+                    .attr('y', d => yScale(d[1]))
+                    .attr('width', barWidth)
+                    .attr('height', d => graphHeight - yScale(d[1]))
+                    .style('fill', '#33adff')
+                    .on('mouseover', function() {
+                      d3.select(this).style('fill', '#ffffff')
+                    })
+                    .on('mouseleave', function() {
+                      d3.select(this).style('fill', '#33adff')
+                    })
+
+  svg.append('g')
+     .call(xAxis)
+     .attr('transform', `translate(${paddingLeft}, ${graphHeight + paddingTop})`)
+
+  svg.append('g')
+     .call(yAxis)
+     .attr('transform', `translate(${paddingLeft}, ${paddingTop})`)
+
+  graph.attr('transform', `translate(${paddingLeft}, ${paddingTop})`)
+
 }
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
